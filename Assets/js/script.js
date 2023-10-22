6c409a251f84301a4e1fd899f60418d42b05f63b
Tony Magrady <tonymagrady@gmail.com>
2023-10-21 22:48:06 -0700

    challenge 06- finished code, fixed button id creation issue, fixed some styling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -10,25 +10,39 @@ const prevCitiesEL = $("#prev-cities");
 const apiKey = "2829bb7521a6e57fb0393e7a19834658";
 let i, city, state, pos, cityNameInp, requestUrl5Day, requestUrlCurr, lattitude, longitude, cityId;
 let weatherDateInp, weatherDateConv, hourAdj, todayDt, iconCode, weatherIconUrl, fiveDayForecast;
-let forecastIdx, windSpeed, humidPerc, foundPrev, idxFirstButton;
+let forecastIdx, windSpeed, humidPerc, foundPrev, idxFirstButton, elemIdShift, responseStatus;
 let reqState = false;
-let reqLatLong = false;
 const currDate = new Date();
 
 //Local storage variables
 let locStorArray = [];
 let cityStage = {id: "", cityState: "", lat: "", long: ""};
 
-//function definitions
+//function declarations
 
 //target functions
 
+prevCitiesEL.on("click", async function(event) {
+  // Prevent the default behavior of refreshing form immediately
+  element = event.target;
+
+  if (element.matches("button")) {
+    // Adjust array index lookup by number of button list shifts (idxFirstButton) after reaching max buttons
+    elemIdShift = element.id.substring(4,5) - idxFirstButton;
+    lattitude = locStorArray[elemIdShift].lat;
+    longitude = locStorArray[elemIdShift].long;
+    requestUrl5Day = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lattitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+    
+    // Call to display current and forecast weather with saved lat and long
+    await get5DayAndCurr();
+  }
+});
+
 async function citySearchSubmit(event) {
-  // Prevent the default behavior
+  // Prevent the default behavior of refreshing form immediately
   event.preventDefault();
 
   cityNameInp = searchCityEl.val().trim();
-  reqLatLong = false;
   reqState = false;
 
   // Check for input errors
@@ -55,9 +69,7 @@ async function citySearchSubmit(event) {
   $('input[type="text"]').val("");
 
   // Construct URL for city-state or just city
-  if (reqLatLong) {
-    requestUrl5Day = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lattitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
-  } else if (reqState) {
+  if (reqState) {
     requestUrl5Day = `https://api.openweathermap.org/data/2.5/forecast/?q=${city},${state},USA&units=imperial&appid=${apiKey}`;
   } else {
     requestUrl5Day = `https://api.openweathermap.org/data/2.5/forecast?q=${city},USA&units=imperial&appid=${apiKey}`;
@@ -74,7 +86,7 @@ async function citySearchSubmit(event) {
     }
   }
   
-  // if city was not already searched, do three or (conditionally) four things listed below:
+  // if city was not already searched, do four or (conditionally) five things listed below:
   if (!foundPrev) {
     // 1. Add to recent searches table
     cityStage.id = cityId;
@@ -83,13 +95,11 @@ async function citySearchSubmit(event) {
     cityStage.long = longitude;
     locStorArray.push(cityStage);
     
-    console.log("locStorArray bef", locStorArray);
-    // 2. Check if one over max - should be at most 8 buttons at one time
+    // 2a and 2b. Check if one over max - should be at most 8 buttons at one time
     if (locStorArray.length >= 9) {
-      // Remove first (oldest) item from array
+      // a. Remove item 0 (oldest item) from array
       locStorArray.shift();
-      console.log("locStorArray aft", locStorArray);
-      // Remove first (oldest) button from page
+      // a. Remove first (oldest) button from page
       $(`#btn-${idxFirstButton}`).remove();
       idxFirstButton++;
     }
@@ -97,10 +107,12 @@ async function citySearchSubmit(event) {
     // 3. Save updated array to storage
     localStorage.setItem("city-searches", JSON.stringify(locStorArray));
 
-    // 4. Display previous search cityStage as buttons from array
-    // renderPrevSearches();
-    prevCitiesEL.append(`<button id="btn-${cityStage.id}">${cityStage.cityState}</button>`);
+    // 4. Add new button to end of list
+    // Index is new last item in array, length - 1. Adjust by idxFirstButton which tracks # of shifts
+    i = locStorArray.length - 1 + idxFirstButton;
+    prevCitiesEL.append(`<button id="btn-${i}">${cityStage.cityState}</button>`);
 
+    // 5. Reinitialize stage record for reuse
     cityStage = {id: "", cityState: "", lat: "", long: ""};
 
     // Create and append button
@@ -108,9 +120,11 @@ async function citySearchSubmit(event) {
 }
 
 function renderPrevSearches () {
+  // Clear section and identify first button as 0 on refresh
   prevCitiesEL.html("");
   idxFirstButton = 0;
 
+  // Build button list on page
   for (i = 0; i < locStorArray.length; i++) {
     let holdCityState = locStorArray[i].cityState;
     prevCitiesEL.append(`<button id="btn-${i}">${holdCityState}</button>`);
@@ -120,16 +134,21 @@ function renderPrevSearches () {
 async function get5DayAndCurr () {
   await get5DayForecast();
 
-  requestUrlCurr = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
-  await getCurrentWeath();
+  if (responseStatus === 200) {
+    requestUrlCurr = `https://api.openweathermap.org/data/2.5/weather?lat=${lattitude}&lon=${longitude}&units=imperial&appid=${apiKey}`;
+    await getCurrentWeath();
+  }
 }
 
 async function get5DayForecast () {
+  console.log("requestUrl5Day", requestUrl5Day);
   try {
     const response = await fetch(requestUrl5Day);
     const data = await response.json();
+    responseStatus = response.status;
     if (!response.ok) {
       console.log(`Open weather call for 5 day forecast: ${response.statusText}`);
+      alert(`Requested city not found: ${cityNameInp}. Message: ${response.statusText}`);
       return;
     }
     //Access result fields
@@ -166,7 +185,6 @@ async function get5DayForecast () {
 
       // Initialize card and display on page
       let forecastDayEl = $(`#forecast-day-${i}`);
-
       forecastDayEl.html("");
       forecastDayEl.append(`<h5>${weatherDateConv}</h5>`);
       weatherIconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
@@ -185,8 +203,10 @@ async function getCurrentWeath () {
   try {
     const response = await fetch(requestUrlCurr);
     const data = await response.json();
+    responseStatus = response.status;
     if (!response.ok) {
       console.log(`Open weather call for current day: ${response.statusText}`);
+      alert(`Requested city not found: ${cityNameInp}. Message: ${response.statusText}`);
       return;
     }
     //Display result fields
@@ -205,73 +225,9 @@ async function getCurrentWeath () {
   }
 }
 
-// // Previous searches and storage management
-// // ----------------------------------------
-
-// function searchNameFormSubmit(event) {
-//   // Prevent the default behavior
-//   event.preventDefault();
-  
-//   // Max saves reached
-//   if (locStorArray.length >= 7) {
-//   }
-
-//   // Save in local storage and internal array
-//   movieEntry.schName = searchName;
-//   movieEntry.movTitle = mTitle;
-//   movieEntry.yr = mYear;
-//   movieEntry.omdbUrl = requestURL;
-//   locStorArray.push(movieEntry);
-  
-//   localStorage.setItem("city-searches", JSON.stringify(locStorArray));
-
-//   // Clear and call to display updated movie searches
-//   renderPrevSearches();
-// }
-
-// prevSearchEl.on("click", function(event) {
-//   element = event.target;
-
-//   if (element.matches("button") && element.className.substring(10,30).trim() === "view-button") {
-//     // View movie info
-//     // Button IDs are in format btnx0 or btnx1 where x is the li row. Element/buttonid[3] is the 4th character.
-//     i = element.id[3];
-//     requestURL = locStorArray[i].omdbUrl
-
-//     renderAllMovieData();
-//   }
-
-//   if (element.matches("button") && element.className.substring(10,30).trim() === "delete-button") {
-//     // Delete saved search (li) row
-//     // Button IDs are in format btnx0 or btnx1 where x is the li row. Element/buttonid[3] is the 4th character.
-//     i = element.id[3];
-//     locStorArray.splice(i,1);
-//     // Call to remove item from local storage
-//     resetLocalStorage();
-
-//     // Redisplay form
-//     renderPrevSearches();
-//   }
-// });
-
-// function resetLocalStorage () {
-//   // Delete local storage, to be reloaded from saved array
-//   localStorage.removeItem("city-searches");
-
-//   // Reload local storage, at present with one less row after delete button
-//   for (i = 0; i < locStorArray.length; i++) {
-//     movieEntry.schName = locStorArray[i].schName;
-//     movieEntry.movTitle = locStorArray[i].movTitle;
-//     movieEntry.yr = locStorArray[i].yr;
-//     movieEntry.omdbUrl = locStorArray[i].omdbUrl;
-  
-//     localStorage.setItem("city-searches", JSON.stringify(locStorArray));
-//   }
-// }
-
-//-----
-// Main
-//-----
+//------------------------
+// Executes on start-up
+//------------------------
 
 // Retrieve city-searches array of "city-searches" objects from storage
 locStorArray = JSON.parse(localStorage.getItem("city-searches"));
@@ -281,30 +237,5 @@ if (locStorArray === null) {
 // Display previous search cities as buttons from array
 renderPrevSearches();
 
+// Await submit
 citySearchEl.on("submit",citySearchSubmit);
-
-
-// - Throwaway
-// const requestURL = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${state}&limit=1&appid=45dc14616eaf796db0039aab6f9100f4`;
-// const requestURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=45dc14616eaf796db0039aab6f9100f4`;
-  
-// function renderPrevSearches () {
-//   console.log("---In renderPrevSearches");
-//   // Clear previous searches display
-//   prevCitiesEL.html("");
-//   console.log("locStorArray in render func:",locStorArray);
-//   // prevSearchEl.addClass("strong-ovrd");  
-
-//   // If there were any stored cities, create them in section 
-//   // Initialize
-//   i = 0;
-//   $.each(locStorArray, function(i) {
-//     prevCitiesEL.append(`<button id="btn-${i}">${locStorArray[i].cityState}</button>`);
-//     console.log("i",i,"locStorArray[i].cityState",locStorArray[i].cityState);
-//   })
-// }
-
-  // let tblIdx = locStorArray.findIndex(function(city) {
-  //   console.log("Found in prev search-,city,",city,"locStorArray:",locStorArray,"cityId:",cityId);  
-  //   return city.id == cityId;
-  // });
--- README.md
+++ README.md
@@ -1,102 +1,57 @@
-# 06 Server-Side APIs: Weather Dashboard
+# Work Day Scheduler - Tony Magrady
 
-## Your Task
+## README Author
+Tony Magrady - Berkeley Full-Stack Web Development Bootcamp student <br>
+-- September 22, 2023
 
-Third-party APIs allow developers to access their data and functionality by making requests with specific parameters to a URL. Developers are often tasked with retrieving data from another application's API and using it in the context of their own. Your challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.
+## Description
+Daily planner calendar webpage (for current day) that allows a user to save events for each hour of a typical working day (9am–5pm). It has color-coded hour intervals to highlight current time of day, before, and after.<br>
+The time slots are text fields for the activities of the day, which can be saved to local storage via save button and retrieved upon page load or refresh:  https://tonymagr.github.io/work-day-scheduler-3rd-party-apis/
 
-Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The base URL should look like the following: `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}`. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
+Module 5 skills and tools that I used in webpage deployment were:
+- HTML Access to CSS frameworks, jQuery library, and Day.js library
+- Day.js formatting
+- Dynamic styling updates with jQuery
+- jQuery event listeners
+- Use of JavaScript forEach method on all target DOM elements that are selected-All
+  - Amninder Singh, classmate of Aug-23 FS Bootcamp, assisted me here
 
-**Hint**: Using the 5 Day Weather Forecast API, you'll notice that you will need to pass in coordinates instead of just a city name. Using the OpenWeatherMap APIs, how could we retrieve geographical coordinates given a city name?
+## Website Image
+![Website Image](./Assets/images/work-day-planner-img.jpg)
 
-You will use `localStorage` to store any persistent data. For more information on how to work with the OpenWeather API, refer to the [Full-Stack Blog on how to use API keys](https://coding-boot-camp.github.io/full-stack/apis/how-to-use-api-keys).
+## Challenges
+I attempted to listen for click events of the Save buttons of all time slots through event delegation on the work-schedule container div and use of event.target, but I was not able to access the element text. I received assistance from a classmate (in Credits below) and changed technique to JavaScript forEach method to loop through selected elements from querySelectorAll. This worked.
 
-## User Story
+## Installation
+N/A
 
-```
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-```
+## Usage
+Employees, managers, non-for-profit team members, and volunteers can use this Work Day Planner to save notable events by time and retrieve them later. It is good for class exercise, but a real-world improvement would be to expand this calendar to future and past days to maximize the use of the stored calendar information.
 
-## Acceptance Criteria
+## Credits
+Amninder Singh, classmate of Aug-23 FS Bootcamp, assisted me on the use of JavaScript forEach method on all target DOM elements that are selected-All. (In this case to detect a click event on any save button in the hourly calendar.)
 
-```
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-```
+## License
+MIT License
 
-## Mock-Up
+Copyright (c) 2023 Tony Magrady
 
-The following image shows the web application's appearance and functionality:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-![The weather app includes a search option, a list of cities, and a five-day forecast and current weather conditions for Atlanta.](./Assets/06-server-side-apis-homework-demo.png)
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-## Grading Requirements
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
-> **Note**: If a Challenge assignment submission is marked as “0”, it is considered incomplete and will not count towards your graduation requirements. Examples of incomplete submissions include the following:
->
-> * A repository that has no code
->
-> * A repository that includes a unique name but nothing else
->
-> * A repository that includes only a README file but nothing else
->
-> * A repository that only includes starter code
-
-This Challenge is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-    * Uses the OpenWeather API to retrieve weather data.
-
-    * Uses `localStorage` to store persistent data.
-
-### Deployment: 32%
-
-* Application deployed at live URL.
-
-* Application loads with no errors.
-
-* Application GitHub URL submitted.
-
-* GitHub repository that contains application code.
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate.
-
-* Application user interface style is clean and polished.
-
-* Application resembles the mock-up functionality provided in the Challenge instructions.
-
-### Repository Quality: 13%
-
-* Repository has a unique name.
-
-* Repository follows best practices for file structure and naming conventions.
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages.
-
-* Repository contains quality readme file with description, screenshot, and link to deployed application.
-
-## Review
-
-You are required to submit BOTH of the following for review:
-
-* The URL of the functional, deployed application.
-
-* The URL of the GitHub repository. Give the repository a unique name and include a readme describing the project.
-
-- - -
-© 2023 edX Boot Camps LLC. Confidential and Proprietary. All Rights Reserved.
+---
